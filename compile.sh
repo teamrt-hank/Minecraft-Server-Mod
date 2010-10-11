c03a8afecaa89ddaf317cef82faccb1622fd6280
mrsheen <info@mrsheenau.com>
2010-10-11 14:55:40 +1000

    Added build script

--- compile.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-javac -sourcepath src -d build/classes -cp bin/minecraft_server.jar src/*.java
-
-jar cvfM export/Minecraft_Mod.jar -C build/classes/ build/classes/*.class -C build/classes/ BanSystem\$Ban.class -C src META-INF
-
-
-
-
